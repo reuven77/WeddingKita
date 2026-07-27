ed74a35efaf15753ee5fcebfc3e78d40a5763ae5
MELONJAWA <slimreuven@gmail.com>
2026-07-27 20:00:51 +0700

    fix: sanitize APP_URL and DATABASE_URL in config to prevent Invalid URI error

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => filter_var(env('APP_URL'), FILTER_VALIDATE_URL) ? env('APP_URL') : 'http://localhost',
 
     /*
     |--------------------------------------------------------------------------
--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL', env('DB_URL')),
+            'url' => filter_var(env('DATABASE_URL'), FILTER_VALIDATE_URL) ? env('DATABASE_URL') : null,
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
